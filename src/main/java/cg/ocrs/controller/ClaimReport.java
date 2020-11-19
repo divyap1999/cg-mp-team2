d7fc0d2a36994223334a4fb520c3bc0d93617f81
Divya Palagummi <divya.palagummi99@gmail.com>
2020-11-19 14:07:32 +0530

    Added Clai Report Generation screen

---
+++ src/main/java/cg/ocrs/controller/ClaimReport.java
@@ -0,0 +1,57 @@
+package cg.ocrs.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import cg.ocrs.dao.ClaimDaoImpl;
+import cg.ocrs.dao.IClaimDao;
+import cg.ocrs.model.Claim;
+
+
+@WebServlet("/report")
+public class ClaimReport extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	IClaimDao service;
+	
+	@Override
+	public void init() throws ServletException {
+		
+		service=new ClaimDaoImpl();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int claimNumber=Integer.parseInt(request.getParameter("claimNumber"));
+//		String claimReason=request.getParameter("claimReason");
+//		String accidentLoactionStreet=request.getParameter("accidentLocationStreet");
+//		String accidentCity = request.getParameter("accidentCity");
+//		String accidentState = request.getParameter("accidentState");
+//		int accidentZip = Integer.parseInt(request.getParameter("accidentZip"));
+//		String claimType = request.getParameter("claimType");
+//		int policyNumber = Integer.parseInt(request.getParameter("policyNumber"));
+		
+		
+		Claim claim = null;
+		try {
+			claim = service.getDetailedClaimReportView(claimNumber);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		HttpSession ssn=request.getSession();
+		ssn.setAttribute("claim", claim);
+		response.sendRedirect("claim-report.jsp");
+		
+		
+	}
+	
+}
---
+++ src/main/webapp/claim-report.jsp
@@ -0,0 +1,49 @@
+<%@page import="java.util.List"%>
+<%@page import="cg.ocrs.model.Claim"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1" isELIgnored="false"%>
+   
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+</div>
+<table class="table">
+	<tr>
+		<th>ClaimNumber</th>
+		<th>ClaimReason</th>
+		<th>Accident Location Street</th>
+		<th>Accident City</th>
+		<th>Accident State</th>
+		<th>Accident ZIP</th>
+		<th>Claim Type</th>
+		<th>Policy Number</th>
+	</tr>
+	
+<% Claim claim = (Claim)session.getAttribute("claim"); %>
+
+	<tr>
+		<td><%=claim.getClaimNumber() %></td>
+		<td><%=claim.getClaimReason() %></td>
+		<td><%= claim.getAccidentLoactionStreet() %></td>
+		<td><%=claim.getAccidentCity() %></td>
+		<td><%= claim.getAccidentState() %></td>
+		<td><%= claim.getAccidentZip() %></td>
+		<td><%= claim.getClaimType() %></td>
+		<td><%= claim.getPolicyNumber() %></td>
+		<td><%=claim.getClaimType() %></td>
+		
+		
+	</tr>
+</table>
+	
+
+</div>
+
+
+</body>
+</html>
